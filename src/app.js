4441a8267aa3ca012a4d9e786721ba4904d1f408
JesseSoldat <jsoldat@hotmail.com>
2018-01-27 16:38:36 +0700

    webpack works

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+console.log('webpack works');
